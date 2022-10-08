ba752bba7a682220fdc94ba5bbe0976d6d5f33a8
Azat Gilyazov <57267956+AnteeOne@users.noreply.github.com>
2022-10-08 15:14:32 +0300

    [004] feature: add counter app

--- App.tsx
+++ App.tsx
@@ -1,20 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {useState} from "react";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+
+    const [count, setCount] = useState(0);
+    const onPress: (isAddition: Boolean) => void = (isAddition: Boolean) => {
+        let beAdded = isAddition ? 1 : -1
+        setCount(prevCount => prevCount + beAdded)
+    };
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.countContainer}>
+                <Text>Count: {count}</Text>
+            </View>
+            <View style={styles.countersContainer}>
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress={() => {
+                        onPress(true)
+                    }}
+                >
+                    <Text>+</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress={() => {
+                        onPress(false)
+                    }}
+                >
+                    <Text>-</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        paddingHorizontal: 16
+    },
+    button: {
+        alignItems: "center",
+        backgroundColor: "#DDDDDD",
+        padding: 16
+    },
+    countContainer: {
+        alignItems: "center",
+        padding: 16
+    },
+    countersContainer: {
+        flexDirection: "row",
+        justifyContent: "space-evenly",
+        padding: 16
+    }
 });
