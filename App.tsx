92b9be74d05fb1c4512663598d70f340db0d3f8e
Azat Gilyazov <57267956+AnteeOne@users.noreply.github.com>
2022-12-03 17:47:42 +0300

    [008] hw: run projects

--- App.tsx
+++ App.tsx
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react'
+import {Button, ScrollView, Text, TextInput, View} from 'react-native'
+import CheckBox from 'expo-checkbox'
+import {observer} from 'mobx-react-lite'
+import Todo from "./src/store/todos/todo";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+// оборачиваем компонент в observer для отслеживания изменений в сторе Mobx
+const App = observer(() => {
+    // создаем хух состояния для инпута
+    const [text, setText] = useState('')
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    return (
+        <ScrollView>
+            {/* Создаем поле для ввода текста задачи */}
+            <TextInput style={{height: 40}} placeholder="Create" onChangeText={t => setText(t)} defaultValue={text}/>
+            {/* Создаем кнопку создания задачи и на onPress вешаем функцию создания задачи в сторе Mobx */}
+            <Button title="Add Todo" onPress={() => Todo.createTodo({id: Math.random(), title: text})}/>
+            {/* Создаем список задач, где получаем их из стора Mobx */}
+            {Todo.todos.map(({id, title, completed}) => (
+                <View
+                    style={{
+                        flexDirection: 'row',
+                        width: 350,
+                        justifyContent: 'space-between',
+                        alignItems: 'center'
+                    }}
+                    key={id}
+                >
+                    {/* Создаем CheckBox выполнения задачи и на onPress вешаем функцию выполнения задачи в сторе Mobx */}
+                    <CheckBox value={completed} onValueChange={() => Todo.completeTodo(id)}/>
+                    <Text>{title}</Text>
+                    {/* Создаем кнопку удаления задачи и на onPress вешаем функцию удаления задачи в сторе Mobx */}
+                    <Button title="Delete" onPress={() => Todo.deleteTodo(id)}/>
+                </View>
+            ))}
+        </ScrollView>
+    )
+})
+
+export default App
