ffadc6139c28bd164d4b682b6f9316cb6de3cf8c
Azat Gilyazov <57267956+AnteeOne@users.noreply.github.com>
2022-12-11 02:10:41 +0300

    [10] feature: add eshop

--- App.tsx
+++ App.tsx
@@ -1,20 +1,96 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Button, StyleSheet} from 'react-native';
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import {NavigationContainer} from "@react-navigation/native";
+import {ProductListScreen} from "./src/screens/products/ProductListScreen";
+import * as React from 'react';
+import {ProductScreen} from "./src/screens/product/ProductScreen";
+import {Colors} from "react-native/Libraries/NewAppScreen";
+import {CartScreen} from "./src/screens/cart/CartScreen";
+import {FavouriteScreen} from "./src/screens/favourite/FavouriteScreen";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+const Stack = createNativeStackNavigator()
+
+const App = () => {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen
+                    name="Products"
+                    component={ProductListScreen}
+                    options={({navigation}) => ({
+                        title: 'Products',
+                        headerLeft: () => (
+                            <Button
+                                onPress={() => navigation.navigate("Favourite")}
+                                title="Favourite"
+                                color={Colors.blue}
+                            />
+                        ),
+                        headerRight: () => (
+                            <Button
+                                onPress={() => navigation.navigate("Cart")}
+                                title="Cart"
+                                color={Colors.blue}
+                            />
+                        ),
+                    })}
+                />
+                <Stack.Screen
+                    name="Product"
+                    component={ProductScreen}
+                    options={({navigation}) => ({
+                        title: 'Product',
+                        headerLeft: () => (
+                            <Button
+                                onPress={() => navigation.popToTop()}
+                                title="Home"
+                                color={Colors.blue}
+                            />
+                        )
+                    })}
+                />
+                <Stack.Screen
+                    name="Cart"
+                    component={CartScreen}
+                    options={({navigation}) => ({
+                        title: 'Cart',
+                        headerLeft: () => (
+                            <Button
+                                onPress={() => navigation.popToTop()}
+                                title="Home"
+                                color={Colors.blue}
+                            />
+                        ),
+                    })}
+                />
+                <Stack.Screen
+                    name="Favourite"
+                    component={FavouriteScreen}
+                    options={({navigation}) => ({
+                        title: 'Favourite',
+                        headerLeft: () => (
+                            <Button
+                                onPress={() => navigation.popToTop()}
+                                title="Home"
+                                color={Colors.blue}
+                            />
+                        ),
+                    })}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
 
+export default App
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        flexDirection: "column",
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,11 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
+module.exports = function (api) {
+    api.cache(true);
+    return {
+        presets: ['module:metro-react-native-babel-preset', 'babel-preset-expo'],
+        plugins: [
+            'react-native-reanimated/plugin',
+            ['@babel/plugin-proposal-decorators', {legacy: true}]
+        ]
+    };
 };
+
